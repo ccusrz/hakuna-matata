21b9c74878a0d0b42627e99db72b9e55cce973cc
ccusrz <66859595+ccusrz@users.noreply.github.com>
2020-09-04 02:47:41 -0400

    Create ADDREV1.py

---
+++ SPOJ/ADDREV1.py
@@ -0,0 +1,11 @@
+#First approach for https://www.spoj.com/problems/ADDREV/
+#ccusrz@2020-09-01@23:31:38
+x = []
+for _ in range(int(input())):
+	sum = 0
+	for l in input().split():
+		sum += int(l[::-1])	
+	x.append(sum)
+ 
+for _ in x:
+	print(str(_)[::-1].strip('0'))
