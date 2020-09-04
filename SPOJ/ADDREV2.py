8e0b42395c02d6081118cd2731da7d274e924b48
ccusrz <66859595+ccusrz@users.noreply.github.com>
2020-09-04 02:45:11 -0400

    Update ADDREV2.py

--- SPOJ/ADDREV2.py
+++ SPOJ/ADDREV2.py
@@ -1,5 +1,5 @@
 #Second approach for https://www.spoj.com/problems/ADDREV/
-#author: ccusrz 2020-09-01 23:53:56
+#ccusrz@2020-09-01@23:53:56
 for _ in range(int(input())):
 	y = input().split()
 	print(int(str(int(y[0][::-1]) + int(y[1][::-1]))[::-1])) 
